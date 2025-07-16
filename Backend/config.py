e2a457b15c56bd31009378f3dc43540868683b20
Abdullah Mohamed <101477947+Abdullah2O7@users.noreply.github.com>
2025-07-16 18:47:39 +0300

    Update config.py to be secure

--- Backend/config.py
+++ Backend/config.py
@@ -2,13 +2,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 class Config:
-    SECRET_KEY = '3da675e0576d436eb0a695855d1a7430'
-    MONGO_URI = 'mongodb+srv://abdullahmohamed1047:HuexVTFWCHGP8wXU@cluster0.ydoaic1.mongodb.net/'
-    SENDER_EMAIL = 'abdullah.mohamed1047@gmail.com'
-    SENDER_PASSWORD = 'ozwd vhmw aada pxnq'
-    GOOGLE_API_KEY="AIzaSyBHP5VbKPIULnM-ZpyM8mL3XIvJd_n5OZ4"
+    # SECRET_KEY = 'Write your security key'
+    # MONGO_URI = 'Write your mongodb url'
+    # SENDER_EMAIL = 'write the sender email'
+    # SENDER_PASSWORD = 'search how to get the password'
+    # GOOGLE_API_KEY="AIzaSyBHP5VbKPIULnM-ZpyM8mL3XIvJd_n5OZ4"
     DEEPSEEK_API_KEY= "sk-6fd2c14211d849b1a86a591ceb5b6fa4"
 
 # mongodb://localhost:27017
-# AIzaSyATVShEBpb9rGD8wDQtkagzzL6nph6N398
-# AIzaSyDF4-cxufrDF0ixCCRoGcXQnIgmk1HCkPQ
